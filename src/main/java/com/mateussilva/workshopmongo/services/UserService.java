4fec143c3762d92fe410ed47b86fd0a885a7c1ba
mateus-hub <theusskt2018@gmail.com>
2022-02-02 12:25:33 -0300

    Deleting a user with DELETE

--- src/main/java/com/mateussilva/workshopmongo/services/UserService.java
+++ src/main/java/com/mateussilva/workshopmongo/services/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
@@ -29,7 +29,13 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
